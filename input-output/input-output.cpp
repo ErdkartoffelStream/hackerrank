82ea2b7f60e3420fb538f1ac93e2931ebad89410
worldpotato <mail@felixstrobel.de>
2021-01-16 15:11:49 +0100

    add main from input output

---
+++ input-output/input-output.cpp
@@ -0,0 +1,28 @@
+/**
+ * @author      : worldpotato
+ * @file        : main
+ * @created     : Sunday Dec 13, 2020 11:59:33 CET
+ */
+
+#include <iostream>
+#include "myMath.hpp"
+
+
+int main()
+{
+    myMath m;
+
+    int x;
+    int y;
+    int z;
+
+    std::cin >> x;
+    std::cin >> y;
+    std::cin >> z;
+
+    int result = m.add3(x, y, z);
+
+    std::cout << result << std::endl;
+}
+
+
